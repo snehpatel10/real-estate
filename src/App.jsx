fc2deb49657a7d3a087619424ace659a0309dc64
Sneh Patel <thisissnehpatel@gmail.com>
2025-01-10 14:41:34 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Home from './pages/Home'
 import Profile from './pages/Profile'
 import Signin from './pages/SignIn'
 import Signup from './pages/Signup'
+import Header from './components/Header'
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path = "/" element = {<Home />} />
         <Route path = "/about" element = {<About />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <header className="bg-slate-200 shadow-md">
+            <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+                <Link to="/">
+                    <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+                        <span className="text-slate-500">True</span>
+                        <span className="text-slate-700">Homes</span>
+                    </h1>
+                </Link>
+                <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        className="bg-transparent focus:outline-none w-24 sm:w-64"
+                    />
+                    <FaSearch className="text-slate-500" />
+                </form>
+                <ul className="flex gap-4">
+                    <Link to="/">
+                        <li className="hidden cursor-pointer sm:inline text-slate-700 hover:text-slate-900 transition-all duration-150">
+                            Home
+                        </li>
+                    </Link>
+                    <Link to="/about">
+                        <li className="hidden cursor-pointer sm:inline text-slate-700 hover:text-slate-900 transition-all duration-150">
+                            About
+                        </li>
+                    </Link>
+                    <Link to="/sign-in">
+                        <li className=" cursor-pointer text-slate-700 hover:text-slate-900 transition-all duration-150">
+                            Sign in
+                        </li>
+                    </Link>
+                </ul>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
