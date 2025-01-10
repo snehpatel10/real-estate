fadf7b31dcbdbebe15537732f10b78a0423552fd
Sneh Patel <thisissnehpatel@gmail.com>
2025-01-10 20:24:14 +0530

    create signin api route

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -59,7 +59,7 @@ function Signup() {
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">
-        Create an account
+        Create your account
       </h1>
       <form className="flex flex-col gap-4"  onSubmit={handleSubmit}>
         <input
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,16 +1,44 @@
-import User from '../models/user.model.js';
-import { errorHandler } from '../utils/error.js';
-import bcryptjs from 'bcryptjs';
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import dotenv from "dotenv";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-export const signup = async(req, res, next) => {
-    const { username, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = User({ username, email, password: hashedPassword });
-    try {
-        await newUser.save();
-        res.status(201).json('User created successfully');
-    } catch (error) {
-        next(error);
+dotenv.config();
+
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
     }
-    
-}
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: userPassword, ...userWithoutPassword } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 10000),
+      })
+      .status(200)
+      .json(userWithoutPassword);
+  } catch (error) {
+    next(error);
+  }
+};
