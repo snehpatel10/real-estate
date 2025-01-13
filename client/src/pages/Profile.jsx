a4da380c7526a2a15bc18f30939da2f8fa3b6f53
Sneh Patel <thisissnehpatel@gmail.com>
2025-01-13 16:35:41 +0530

    complete update user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,41 +1,123 @@
-import React, { useState, useRef, useEffect } from 'react'
-import {useSelector} from 'react-redux'
+import React, { useState, useRef, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { updateUserStart, updateUserSuccess, updateUserFailure } from "../features/userSlice";
+import toast, { Toaster } from 'react-hot-toast'
 
 function Profile() {
-  const fileRef = useRef(null)
-  const {currentUser} = useSelector(state => state.user)
-  const [file,setFile] = useState(undefined)
-  console.log(file)
+  const fileRef = useRef(null);
+  const { currentUser, loading } = useSelector((state) => state.user);
+  const [file, setFile] = useState(undefined);
+  const [formData, setFormData] = useState({});
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    if(file) {
+  useEffect(() => {
+    setFormData({
+      username: currentUser.username,
+      email: currentUser.email,
+      password: '',
+    });
+  }, [currentUser]);
+
+  useEffect(() => {
+    if (file) {
       handleFileUpload();
     }
   }, [file]);
 
-  const handleFileUpload = () => {
-    
-  }
+  const handleFileUpload = () => {};
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        toast.error("An error occurred. Please try again.");
+      }
+
+      dispatch(updateUserSuccess(data.rest));
+      toast.success("Changes saved successfully!");
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+      toast.error("Internal server error. Please try again later.");
+    }
+  };
+
+  const isFormChanged = () => {
+    return (
+      formData.username !== currentUser.username ||
+      formData.email !== currentUser.email ||
+      formData.password !== ''
+    );
+  };
 
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl font-semibold text-center my-7'>
-        Profile
-      </h1>
-      <form className='flex flex-col gap-4'>
-        <input onChange={(e)=>setFile(e.target.files[0])} type="file" ref={fileRef} hidden accept='image/*'/>
-        <img onClick={()=>fileRef.current.click()} src={currentUser.avatar} alt="profile" className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'/>
-        <input type="text" placeholder='username' id='username' className='border p-3 rounded-lg' />
-        <input type="text" placeholder='email' id='email' className='border p-3 rounded-lg' />
-        <input type="text" placeholder='passowrd' id='password' className='border p-3 rounded-lg' />
-        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:bg-slate-800 disabled:opacity-80 transition-all duration-150'>Update</button>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={fileRef}
+          hidden
+          accept="image/*"
+        />
+        <img
+          onClick={() => fileRef.current.click()}
+          src={currentUser.avatar}
+          alt="profile"
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          value={formData.username}
+          id="username"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="email"
+          value={formData.email}
+          id="email"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="border p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading || !isFormChanged()}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:bg-slate-800 disabled:opacity-50 transition-all disabled:cursor-not-allowed disabled:bg-slate-800 duration-150"
+        >
+          {loading ? 'Loading...' : 'Update'}
+        </button>
       </form>
-      <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
+      <Toaster />
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
